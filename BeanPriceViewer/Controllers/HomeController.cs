6226511fb9e96e9dfefa1778832f124c928aea44
dylan <dylanwuu@outlook.com>
2025-11-28 13:18:50 -0800

    nuget packages + first work on views

--- BeanPriceViewer/Controllers/HomeController.cs
+++ BeanPriceViewer/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace BeanPriceViewer.Controllers
             return View();
         }
 
+        public IActionResult CityView()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
